3e5c0eb5f1289740ac803cd8a9240a2a26204a0e
Matei Albu <77253554+Matei-Albu@users.noreply.github.com>
2026-03-01 21:09:01 -0500

    Create 1022-Sum-of-Root-To-Leaf-Binary-Numbers.py

---
+++ 1022-Sum-of-Root-To-Leaf-Binary-Numbers.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        def sum_root(node, current ):
+            if not node:
+                return 0
+            
+            current = current * 2 + node.val
+
+            if not node.left and not node.right:
+                return current
+
+            return sum_root(node.left, current) + sum_root(node.right,current)
+        return sum_root(root,0)
+
