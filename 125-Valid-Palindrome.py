fe69b8771b01fde2e9cd8a261d7b5128b688b03d
Matei Albu <77253554+Matei-Albu@users.noreply.github.com>
2026-03-24 21:06:08 -0400

    Update 125-Valid-Palindrome.py

--- 125-Valid-Palindrome.py
+++ 125-Valid-Palindrome.py
@@ -1,14 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         l, r = 0, len(s) - 1
-        alphanumeric = set("abcdefghijklmnopqrstuvwxyz1234567890")
+        alphanumeric = "0123456789abcdefghijklmnopqrstuvwxyz"
 
         while l < r:
-            while l < r and s[r].lower() not in alphanumeric:
+            if s[l].lower() not in alphanumeric:
+                l +=1
+            elif s[r].lower() not in alphanumeric:
                 r -= 1
-            while l < r and s[l].lower() not in alphanumeric:
-                l += 1
-            if s[r].lower() != s[l].lower():
+            elif s[l].lower() != s[r].lower():
                 return False
-            l,r = l + 1, r - 1
+            else:
+                l +=1
+                r -=1
         return True
